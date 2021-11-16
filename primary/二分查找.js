f4d7387bcc6fedeb3ea90394d54b36dc17740849
杨旭 <15801593841@163.com>
2021-11-16 22:32:45 +0800

    提交代码

--- primary/二分查找.js
+++ primary/二分查找.js
@@ -32,4 +32,47 @@
     var arr = [5,13,19,21,37,56,64,75,80,88,92]
     var index = binary_search(arr, 21)
     console.log(index)
-    
+// 递归方法
+    function binary_search (arr, start, end, key) {
+        if( start > end ){
+            return -1
+        }
+        var mid = start + (( end - start ) >> 1 )
+        if( arr[mid] == key ){
+            return mid
+        }else if( key > arr[mid] ){
+            start = mid + 1
+            return binary_search(arr, start, end, key)
+        }else if( key < arr[mid] ){
+            end = mid -1
+            return binary_search(arr, start, end, key)
+        }
+    }
+    var arr = [5,13,19,21,37,56,64,75,80,88,92]
+    var index = binary_search(arr, 0, arr.length-1, 21)
+    console.log(index)
+
+// 如果有重复的数字，进行的兼容
+function binary_search ( arr, key) {
+    var start = 0 ,end = arr.length -1;
+    while ( start <= end ){
+        // 获取中间值
+        var mid = start + ( (end - start) >> 1 )
+        if( arr[mid] > key ) {
+            // 如果中间值大于给定值就将末尾值等于中间值-1
+            end = mid -1
+        }else if( arr[mid] < key ) {
+            // 如果中间值小于给定值就将起始值等于中间值+1
+            start = mid +1
+        }else{
+            // 针对重复数字相等的问题，如果不等于最后一个，或者不等于
+            if( (mid == arr.length -1) || (arr[mid+1] != key)){
+                return mid
+            }else {
+                low = mid + 1
+            }
+        }
+    }
+
+}
+// 以上就是二分法的核心知识点了。
